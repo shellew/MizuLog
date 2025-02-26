b57259b48c91d83dd903d16b822f7265c915296f
shellew <nishiokaryouichi2020@MacBook-Air.local>
2025-02-26 14:51:54 +0900

    app.py: 本番用アプリ実行

--- app.py
+++ app.py
@@ -222,4 +222,6 @@ def static_files(filename):
 
 # アプリの実行
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    init_db()
+    app.run(host="0.0.0.0", port=port)
